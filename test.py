55fdbf0f63c061f1844a70c9bc6c31f20fc36269
byunghyun.bang <byunghyun.bang@linecorp.com>
2022-10-21 01:55:41 +0900

    Change to pykorbit

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import pykorbit
 
-key = "xxx"          # 본인 값으로 변경
-secret = "xxx"          # 본인 값으로 변경
+key = "4hWIjWfC7GSf9MHllleMrLn4LzYYDpZdS7fTLJpRqH6Jc79M0ELWqVrODpn0u"          # 본인 값으로 변경
+secret = "aD0xZGOnePgVIeGItBiT0n10OfauxR1CKDwHhgPuGJkYPIJMDQBumuEwpzMd0"          # 본인 값으로 변경
 korbit = pykorbit.Korbit(key=key, secret=secret)
 
 balances = korbit.get_balances()
 print(balances)
 print(balances["btc"])         # KRW-BTC 조회
-print(balances["KRW"])         # 보유 현금 조회
+print(balances["krw"])         # 보유 현금 조회
