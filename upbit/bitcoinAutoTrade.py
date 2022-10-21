72dabb75a6abec28d6fe9eed713505b93640cbb5
byunghyun.bang <byunghyun.bang@linecorp.com>
2022-10-22 02:02:06 +0900

    Fix exit logic

--- upbit/bitcoinAutoTrade.py
+++ upbit/bitcoinAutoTrade.py
@@ -65,11 +65,11 @@ while True:
 
             # 이익실현 시점에 50% 매도
             if (not is_exit) and exit_price < current_price:
-            exit_btc = get_balance("BTC") * 0.5
-            if btc > 0.00008:
-                log("exit: current_price={}, exit_price={}, exit_btc={}".format(current_price, target_price, exit_btc))
-                upbit.sell_market_order("KRW-BTC", exit_btc)
-                is_exit=True
+                exit_btc = get_balance("BTC") * 0.5
+                if btc > 0.00008:
+                    log("exit: current_price={}, exit_price={}, exit_btc={}".format(current_price, target_price, exit_btc))
+                    upbit.sell_market_order("KRW-BTC", exit_btc)
+                    is_exit=True
         else:
             btc = get_balance("BTC")
             if btc > 0.00008:
