8da18d333ab7ac6bf6595a5e83462b3c81c771a2
byunghyun.bang <byunghyun.bang@linecorp.com>
2022-11-10 23:55:27 +0900

    ...

--- upbit/trading_settings.py
+++ upbit/trading_settings.py
@@ -1,8 +1,11 @@
 symbol="ETH"
 k = 0.4
 expected_rate_p = 2.0
-partial_sell_rate = 1.0 # 익절시 매도비율
+partial_sell_rate_p = 100 # 익절시 매도비율 %
 emergency_sell_rate_p = 2.5
 # candle_interval="minute60"
 candle_interval="minute240"
+# candle_interval="minute10"
+# candle_interval="minute5"
+# candle_interval="minute1"
 # candle_interval="day"
