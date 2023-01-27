15988f4ef8ed05c9bb2f23baf130a2410d589ece
byunghyun.bang <byunghyun.bang@linecorp.com>
2023-01-27 10:47:29 +0900

    Modify start.sh

--- upbit/start.sh
+++ upbit/start.sh
@@ -1,23 +1,30 @@
 nohup python3 -u cryptoAutoTrade.py --symbol=1INCH >> out-1INCH.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=AHT >> out-AHT.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=APT >> out-APT.log &
+sleep 1
 nohup python3 -u cryptoAutoTrade.py --symbol=ARDR >> out-ARDR.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=AXS >> out-AXS.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=BORA >> out-BORA.log &
+sleep 1
 nohup python3 -u cryptoAutoTrade.py --symbol=CBK >> out-CBK.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=CELO >> out-CELO.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=GAS >> out-GAS.log &
+sleep 1
 nohup python3 -u cryptoAutoTrade.py --symbol=GMT >> out-GMT.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=HUNT >> out-HUNT.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=ICX >> out-ICX.log &
+sleep 1
 nohup python3 -u cryptoAutoTrade.py --symbol=LOOM >> out-LOOM.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=MED >> out-MED.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=MLK >> out-MLK.log &
+sleep 1
 nohup python3 -u cryptoAutoTrade.py --symbol=OMG >> out-OMG.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=REP >> out-REP.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=SOL >> out-SOL.log &
+sleep 1
 nohup python3 -u cryptoAutoTrade.py --symbol=STPT >> out-STPT.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=STRK >> out-STRK.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=UPP >> out-UPP.log &
+sleep 1
 nohup python3 -u cryptoAutoTrade.py --symbol=WAXP >> out-WAXP.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=XTZ >> out-XTZ.log &
