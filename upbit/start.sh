29f6e2efdefe099596fe5ea7610ae4bba36d112a
byunghyun.bang <byunghyun.bang@linecorp.com>
2023-01-27 19:10:07 +0900

    Modify start.sh

--- upbit/start.sh
+++ upbit/start.sh
@@ -2,27 +2,27 @@ nohup python3 -u cryptoAutoTrade.py --symbol=1INCH >> out-1INCH.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=AHT >> out-AHT.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=APT >> out-APT.log &
 sleep 1
-nohup python3 -u cryptoAutoTrade.py --symbol=ARDR >> out-ARDR.log &
+nohup python3 -u cryptoAutoTrade.py --symbol=ARDR --min_volume_to_buy=3000000 >> out-ARDR.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=AXS >> out-AXS.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=BORA >> out-BORA.log &
 sleep 1
-nohup python3 -u cryptoAutoTrade.py --symbol=CBK >> out-CBK.log &
+nohup python3 -u cryptoAutoTrade.py --symbol=CBK --min_volume_to_buy=5000000 >> out-CBK.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=CELO >> out-CELO.log &
-nohup python3 -u cryptoAutoTrade.py --symbol=GAS >> out-GAS.log &
+nohup python3 -u cryptoAutoTrade.py --symbol=GAS --min_volume_to_buy=1000000 >> out-GAS.log &
 sleep 1
 nohup python3 -u cryptoAutoTrade.py --symbol=GMT >> out-GMT.log &
-nohup python3 -u cryptoAutoTrade.py --symbol=HUNT >> out-HUNT.log &
+nohup python3 -u cryptoAutoTrade.py --symbol=HUNT --min_volume_to_buy=1000000 >> out-HUNT.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=ICX >> out-ICX.log &
 sleep 1
 nohup python3 -u cryptoAutoTrade.py --symbol=LOOM >> out-LOOM.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=MED >> out-MED.log &
-nohup python3 -u cryptoAutoTrade.py --symbol=MLK >> out-MLK.log &
+nohup python3 -u cryptoAutoTrade.py --symbol=MLK --min_volume_to_buy=10000000 >> out-MLK.log &
 sleep 1
 nohup python3 -u cryptoAutoTrade.py --symbol=OMG >> out-OMG.log &
-nohup python3 -u cryptoAutoTrade.py --symbol=REP >> out-REP.log &
+nohup python3 -u cryptoAutoTrade.py --symbol=REP --min_volume_to_buy=1000000 >> out-REP.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=SOL >> out-SOL.log &
 sleep 1
-nohup python3 -u cryptoAutoTrade.py --symbol=STPT >> out-STPT.log &
+nohup python3 -u cryptoAutoTrade.py --symbol=STPT --min_volume_to_buy=50000000 >> out-STPT.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=STRK >> out-STRK.log &
 nohup python3 -u cryptoAutoTrade.py --symbol=UPP >> out-UPP.log &
 sleep 1
